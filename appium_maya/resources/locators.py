cea5014fac9790e6d261a8ca9cb059aa37996603
Rakibul Alam Shuvo <54779883+shuvoprime@users.noreply.github.com>
2022-02-08 00:50:45 +0600

    adding  POM style scripts

---
+++ appium_maya/resources/locators.py
@@ -0,0 +1,8 @@
+class common_locators:
+    skip_tour_id = "com.maya.mayaapaapp.qa:id/skipBtn"
+    language_dropdown_bar_id = "com.maya.mayaapaapp.qa:id/lang_toggle_text_view"
+    language_english_radio_button_id = "com.maya.mayaapaapp.qa:id/lang_english_radio_btn"
+    language_ok_button_id = "com.maya.mayaapaapp.qa:id/ok_btn"
+
+class homepage_locators:
+    homepage_header_id = "com.maya.mayaapaapp.qa:id/lbl_hi_text_view"
